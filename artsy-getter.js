4e7ebfadbfbce6e73283dd38f0a67c42c053b9db
Bon Crowder @MathFour <mathfour.com@gmail.com>
2017-06-21 11:14:35 -0500

    all file names working in tandem

--- artsy-getter.js
+++ artsy-getter.js
@@ -30,7 +30,7 @@ var fileMoniker = 'artists'; // bmc: the file to be written
 var urlFocus = '/artists'; // bmc: suffix on the baseURL above that will be called
 var maxRecordsAtATime = 10; // bmc: each page will have this many records
 var maxChunksAtATime = 2; // bmc: each call will do this many pages
-var offSetForThisBatch = 0; // bmc: if we want to offset todo
+var offSetForThisBatch = 20; // bmc: if we want to offset todo
 var fileType = 'csv'; // bmc: file extension/type to save to
 
 // bmc: ***************************************************
@@ -39,38 +39,28 @@ var fileType = 'csv'; // bmc: file extension/type to save to
 
 // bmc: file name that gives all the info, BUT it still needs the correct extension
 // bmc: this might be better somewhere else in a smaller scope. Maybe. todo
-var batchTitle = fileMoniker + '_Records' + maxRecordsAtATime + '_Chuncks' + maxChunksAtATime + '_Offset' + offSetForThisBatch;
+var batchTitle = fileMoniker + '_Records' + maxRecordsAtATime + '_Chunks' + maxChunksAtATime + '_Offset' + offSetForThisBatch;
 
 // bmc: url to get the number of records
 var getCountUrl = urlFocus + "?total_count=1";
 
 // bmc: gets the count, creates the file and then comes back to cycle through and save the records as a file
 getList(getCountUrl, function (count) {
+    // bmc: instead of going through all 77K or so records, this lets us go through selected small chunks
     var stopAfter = (maxChunksAtATime * maxRecordsAtATime);
     for (var i = 0; i < stopAfter; i += maxRecordsAtATime) {
-
-        console.log('counter is at', i);
-
-// bmc: there are four instances of "instance.get()" here - 3 are commented out
-// bmc: this is for debugging
-
-        // var urlOffset = urlFocus + '?offset=' + i;
-        // instance.get(urlOffset)
-
-        // var urlSize = urlFocus + '?size=' + maxRecordsAtATime;
-        // instance.get(urlSize)
-
-        // instance.get(urlFocus)
-        // bmc: this is for the "plain" call
-
-        var urlFocusOffset = urlFocus + "?offset=" + i + '&size=' + maxRecordsAtATime;
+        // bmc: this lets us get a few small chunks starting at the requested offset from the above user adjustable variables
+        var offsetAmount = offSetForThisBatch + i;
+        var urlFocusOffset = urlFocus + "?offset=" + offsetAmount + '&size=' + maxRecordsAtATime;
         console.log('urlFocusOffset (the url that is being accessed) is', urlFocusOffset);
 
+        // bmc: actually getting that data and writing it to a file
         instance.get(urlFocusOffset)
         .then(function (response) {
-            console.log('this works ***** this works ***** this works ***** this works'.rainbow);
+            console.log('this works ***** this works ***** this works ***** this works'.rainbow); // bmc: for those who need instant gratification
 
             for (var k = 0; k < maxRecordsAtATime; k++) {
+                // bmc: these seemed to be reasonable bits of to save
                 var saveThisOne =
                         response.data._embedded.artists[k].id + ', ' +
                         response.data._embedded.artists[k].slug + ', ' +
@@ -83,29 +73,21 @@ getList(getCountUrl, function (count) {
                         response.data._embedded.artists[k].location + ', ' +
                         response.data._embedded.artists[k].hometown + '\r';
 
-                var useThisFileName = batchTitle + '.' + fileType;
+                // bmc: create the file name using the batchTitle and the proper file extension
+                var useThisFileName = batchTitle + '_CurrentTotalCount' + count + '.' + fileType;
+
+                // bmc: and writing the info to the file (woohoo!)
                 fs.appendFile(useThisFileName, saveThisOne, function () {
-                    console.log('Record has been appended');
+                    console.log('Record has been appended'.magenta); // bmc: again, gratification
                 })
             }
 
         }).catch(function (error) {
-            console.log(getRandomIntInclusive(10000, 100000), "ERROR ON LOOP:", JSON.stringify(error.data));
+            // bmc: just in case we get crap-tons of errors, this helps us see that they are different
+            console.log(getRandomIntInclusive(10000, 100000), "ERROR ON LOOP:", error.code);
         });
     }
 });
-
-// Initiate the listener
-app.listen(PORT, function () {
-    console.log('listening on port', PORT);
-});
-
-// bmc: number randomizer for identity help
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 // bmc: getList gets the number of records available for this, then returns the count to the callback function - that function embedded above
 function getList(getCountUrlFocus, callback) {
@@ -119,12 +101,25 @@ function getList(getCountUrlFocus, callback) {
 
         var useThisFileName = batchTitle + '_CurrentTotalCount' + totalRecordCount + '.' + fileType;
 
+        // bmc: saving the column names and creating the file for all the data so we can get it into excel/google sheets
         fs.writeFile(useThisFileName, columnHeaders, function () {
             console.log(useThisFileName, 'has been created');
 
             callback(totalRecordCount);
         })
     }).catch(function (error) {
-        console.log("bummer, you got an error:", error.code);
+        console.log(getRandomIntInclusive(10000, 100000), "bummer, you got an error:", error.code);
     });
 }
+
+// bmc: number randomizer for identity help
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// bmc: time to listen...
+app.listen(PORT, function () {
+    console.log('listening on port', PORT);
+});
