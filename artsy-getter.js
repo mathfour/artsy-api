b640fbed2a8bbca52bfe3696200b108e48efa691
Bon Crowder @MathFour <mathfour.com@gmail.com>
2017-06-21 10:49:01 -0500

    finally got offset and size working together

--- artsy-getter.js
+++ artsy-getter.js
@@ -1,6 +1,10 @@
-/**
- * Created by SilverDash on 6/12/17.
- */
+// bmc: Created by Bon Crowder, June 2017
+// bmc: all of my comments are marked with my initials, "bmc"
+// bmc: questions? give me a call at 713-557-8048
+
+// bmc: ***************************************************
+// bmc: Dependencies
+// bmc: ***************************************************
 var express = require("express");
 var path = require("path");
 var fs = require('fs');
@@ -12,7 +16,6 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 var instance = axios.create({
     baseURL: 'https://api.artsy.net/api',
-    // timeout: 1000,
     headers: {
         'X-XAPP-Token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsImV4cCI6MTQ5ODU4OTA1MCwiaWF0IjoxNDk3OTg0MjUwLCJhdWQiOiI1OTQyZTFiNDhiM2I4MTA5ZmJmODZjMjQiLCJpc3MiOiJHcmF2aXR5IiwianRpIjoiNTk0OTZjZmExMzliMjE2ZDYwZTBkNTAxIn0.yk2krkVaoBsXT5c7TVA28bR-Vy-N_JQw2uBYmXVTr3I',
         'Accept': 'application/vnd.artsy-v2+json'
@@ -23,40 +26,49 @@ var instance = axios.create({
 // bmc: ************** user can change these **************
 // bmc: ***************************************************
 
-var appendFileName = 'artists'; // bmc: the file to be written
+var fileMoniker = 'artists'; // bmc: the file to be written
 var urlFocus = '/artists'; // bmc: suffix on the baseURL above that will be called
 var maxRecordsAtATime = 10; // bmc: each page will have this many records
 var maxChunksAtATime = 2; // bmc: each call will do this many pages
-var offSetForThisBatch = 0; // bmc:
+var offSetForThisBatch = 0; // bmc: if we want to offset todo
+var fileType = 'csv'; // bmc: file extension/type to save to
 
 // bmc: ***************************************************
 // bmc: ***************************************************
 // bmc: ***************************************************
 
+// bmc: file name that gives all the info, BUT it still needs the correct extension
+// bmc: this might be better somewhere else in a smaller scope. Maybe. todo
+var batchTitle = fileMoniker + '_Records' + maxRecordsAtATime + '_Chuncks' + maxChunksAtATime + '_Offset' + offSetForThisBatch;
 
-var getCountUrl = urlFocus + "?total_count=1"; // bmc: url to get the number of records
+// bmc: url to get the number of records
+var getCountUrl = urlFocus + "?total_count=1";
 
+// bmc: gets the count, creates the file and then comes back to cycle through and save the records as a file
 getList(getCountUrl, function (count) {
     var stopAfter = (maxChunksAtATime * maxRecordsAtATime);
     for (var i = 0; i < stopAfter; i += maxRecordsAtATime) {
 
         console.log('counter is at', i);
 
-        console.log('urlFocus is', urlFocus);
+// bmc: there are four instances of "instance.get()" here - 3 are commented out
+// bmc: this is for debugging
 
-        var urlFocusOffset = urlFocus + "?offset=" + i + '?size=100';
-        console.log('urlFocusOffset is', urlFocusOffset);
+        // var urlOffset = urlFocus + '?offset=' + i;
+        // instance.get(urlOffset)
 
-        var urlSize = urlFocus + '?size=' + maxRecordsAtATime;
+        // var urlSize = urlFocus + '?size=' + maxRecordsAtATime;
+        // instance.get(urlSize)
 
-        instance.get(urlSize)
         // instance.get(urlFocus)
-        // instance.get(urlFocusOffset)
-        .then(function (response) {
-            console.log('this works');
-            // console.log('the response.data._embedded.artists[0] before stringify is this'.red, response.data._embedded.artists[0]);
+        // bmc: this is for the "plain" call
 
-            // var saveThis = JSON.stringify(response.data);
+        var urlFocusOffset = urlFocus + "?offset=" + i + '&size=' + maxRecordsAtATime;
+        console.log('urlFocusOffset (the url that is being accessed) is', urlFocusOffset);
+
+        instance.get(urlFocusOffset)
+        .then(function (response) {
+            console.log('this works ***** this works ***** this works ***** this works'.rainbow);
 
             for (var k = 0; k < maxRecordsAtATime; k++) {
                 var saveThisOne =
@@ -71,18 +83,17 @@ getList(getCountUrl, function (count) {
                         response.data._embedded.artists[k].location + ', ' +
                         response.data._embedded.artists[k].hometown + '\r';
 
-                fs.appendFile(appendFileName, saveThisOne, function () {
-                    console.log(appendFileName, 'has been appended');
+                var useThisFileName = batchTitle + '.' + fileType;
+                fs.appendFile(useThisFileName, saveThisOne, function () {
+                    console.log('Record has been appended');
                 })
             }
 
-
         }).catch(function (error) {
-            console.log(getRandomIntInclusive(10000, 100000), "ERROR ON LOOP:", JSON.stringify(error));
+            console.log(getRandomIntInclusive(10000, 100000), "ERROR ON LOOP:", JSON.stringify(error.data));
         });
     }
-})
-;
+});
 
 // Initiate the listener
 app.listen(PORT, function () {
@@ -102,15 +113,18 @@ function getList(getCountUrlFocus, callback) {
     instance.get(getCountUrlFocus)
     .then(function (response) {
         var totalRecordCount = JSON.stringify(response.data.total_count);
-        console.log('this should be the count:', totalRecordCount);
+
+        // bmc: over the long term, these should be connected to the actual outputs so changing one here doesn't screw the pooch for the whole file - todo
         var columnHeaders = 'id, slug, name, sortable_name, gender, created_at, updated_at, nationality, location, hometown \r';
 
-        fs.writeFile(appendFileName, columnHeaders, function () {
-            console.log(appendFileName, 'has been created');
+        var useThisFileName = batchTitle + '_CurrentTotalCount' + totalRecordCount + '.' + fileType;
+
+        fs.writeFile(useThisFileName, columnHeaders, function () {
+            console.log(useThisFileName, 'has been created');
 
             callback(totalRecordCount);
-            })
-        }).catch(function (error) {
-            console.log("bummer, you got an error:", error.code);
-        });
-    }
+        })
+    }).catch(function (error) {
+        console.log("bummer, you got an error:", error.code);
+    });
+}
